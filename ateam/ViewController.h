fd195b994e2ede1f826b954db89e7ad8815a5614
John Mead <johnmmead@gmail.com>
2015-02-26 10:32:09 -0800

    Initial Commit

---
+++ ateam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ateam
+//
+//  Created by John Mead on 2/26/15.
+//  Copyright (c) 2015 Ancestry.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
