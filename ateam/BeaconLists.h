f8e86c9e22b1e953be7829bda8d00873ba75f2af
Shengzhe Chen <schen@ancestry.com>
2015-02-26 11:46:13 -0800

    setup beacon

---
+++ ateam/BeaconLists.h
@@ -0,0 +1,21 @@
+//
+//  BeaconLists.h
+//  ateam
+//
+//  Created by Shengzhe Chen on 2/26/15.
+//  Copyright (c) 2015 Ancestry.com. All rights reserved.
+//
+
+#ifndef ateam_BeaconLists_h
+#define ateam_BeaconLists_h
+
+static NSString *purpleUUID = @"B9407F30-F5F8-466E-AFF9-25556B57FE6D";
+static uint16_t purpleMajor = 14500;
+static uint16_t purpleMinor = 14502;
+static NSString *purpleIdentifier = @"Purple Stone";
+
+static NSString *blueUUID = @"";
+static NSString *blueMajor = @"";
+static NSString *blueMinor = @"";
+
+#endif
---
+++ ateam/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  ateam
+//
+//  Created by Shengzhe Chen on 2/26/15.
+//  Copyright (c) 2015 Ancestry.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
